b40c59e8398525c99925343cbc0e4b0bb7e54af9
Toni <tfarina58@gmail.com>
2023-05-06 20:19:41 +0200

    3rd commit

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -4,7 +4,6 @@ import {CommonModule} from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
-
 @NgModule({
   declarations: [],
   imports: [
