44438c07ba8b6f7891184db6224c24d305abb0bb
Toni <tfarina58@gmail.com>
2023-05-07 16:48:59 +0200

    Customer interface

--- src/app/interfaces/customer/customer.ts
+++ src/app/interfaces/customer/customer.ts
@@ -11,3 +11,17 @@ export interface Company {
   country?: string,
   phone: string
 }
+
+export interface Individual {
+  id: string,
+  firstname: string,
+  lastname: string,
+  birthday: string,
+  email: string,
+  typeOfCustomer: string,
+  street?: string,
+  zip?: string,
+  city?: string,
+  country?: string,
+  phone?: string
+}
